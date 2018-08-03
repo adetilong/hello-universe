33a20f604685d416a9bf3620644346fbe9d248f7
adetilong <41926453+adetilong@users.noreply.github.com>
2018-08-03 04:39:48 +0100

    Button Send to right
    issue #1549 solution

---
+++ main.js
@@ -0,0 +1,21 @@
+﻿
+var imgObj = null;
+function init() {
+    imgObj = document.getElementById('btnSend');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 1200 + 'px';
+
+}
+
+function InitMove() {
+    init();
+    moveRight();
+}
+window.onload = InitMove;
+
+
+
+
